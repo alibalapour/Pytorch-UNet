381dad110dcf967b7aa10ebb661d366292746ab4
Ali Balapour <42287060+alibalapour@users.noreply.github.com>
2022-02-01 18:24:53 +0330

    Update train.py

--- train.py
+++ train.py
@@ -90,6 +90,10 @@ def train_net(net,
 
                 with torch.cuda.amp.autocast(enabled=amp):
                     masks_pred = net(images)
+                    
+                    print('masks_pred shape :', masks_pred.shape)
+                    print('true_masks shape :', true_masks.shape)
+                    
                     loss = criterion(masks_pred, true_masks) \
                            + dice_loss(F.softmax(masks_pred, dim=1).float(),
                                        F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float(),
